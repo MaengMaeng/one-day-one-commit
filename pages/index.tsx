834e0b618fe28340144234cf2cf63602b8a0d5e0
minimal1 <jcm940308@gmail.com>
2020-08-13 00:51:02 +0900

    Add styled-components and write readme.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,14 @@ interface IProps {}
 const Index: React.FC<IProps> = () => {
   return (
     <div>
-      <input type="checkbox" checked={true} readOnly />
-      <span>eslint & prettier setting</span>
+      <div>
+        <input type="checkbox" checked={true} readOnly />
+        <span>eslint & prettier setting</span>
+      </div>
+      <div>
+        <input type="checkbox" checked={true} readOnly />
+        <span>Write readme & add styled-components</span>
+      </div>
     </div>
   );
 };
